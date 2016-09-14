2e42fc936bd0965294fb4ea376211bcf2ce61d8c
Michael Pollind <mpollind@gmail.com>
2016-09-14 11:27:53 -0700

    fixed archive

--- archive.php
+++ archive.php
@@ -5,39 +5,10 @@
         <div class="small-12  medium-9 large-expand columns">
             <?php
             if (have_posts())
-            { 
-
-                // $post = $posts[0];
-                // echo "<div class=\"archive-title\">";
-                // if (is_category()) {
-                //     echo "Archive for the &#8216;" .single_cat_title(); . "&#8217; Category";
-                // } 
-                // elseif( is_tag() ) { 
-                //     echo "Archive for the &#8216;".single_cat_title(); ."&#8217; Category";
-                // } 
-                // elseif (is_day()) {
-                //     echo "Archive for " . the_time('F jS, Y');
-
-                // } 
-                // elseif (is_month()) {
-                //     echo "Archive for " . the_time('F, Y');
-
-                // } 
-                // elseif (is_year()) {
-                //     echo "Archive for " . the_time('Y');
-
-                // } 
-                // elseif (is_author()) {
-                //  echo "Author Archive";
-
-                // } 
-                // elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {
-                //     echo "Blog Archives";
-                // }
-                // echo "</div>";
-                
-
-
+            {
+                the_archive_title( '<div class="archive-title">', '</div>' );
+                the_archive_description( '<div class="taxonomy-description">', '</div>' );
+            
                 
                 while (have_posts())
                 { 
